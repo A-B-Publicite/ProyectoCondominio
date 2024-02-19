c1d55a295202b5cc36db79bbd35b52fd72f96adf
jonathanpoaquiza <jonathan.poaquiza@epn.edu.ec>
2024-02-19 09:32:12 -0500

    parq

--- ProyectoCondominio/src/main/java/Inmueble/Departamento.java
+++ ProyectoCondominio/src/main/java/Inmueble/Departamento.java
@@ -3,15 +3,21 @@ package Inmueble;
 import Administracion.Perfil;
 
 public class Departamento extends InmueblePrivado {
+    ParqueaderoPrivado parqueaderPrivado;
+
     public Departamento(String nombre, double metrosCuadrados, boolean necesitaMantenimiento, double alicuota, Perfil propietario) {
         super(nombre, metrosCuadrados, necesitaMantenimiento, alicuota, propietario);
+        this.parqueaderPrivado = new ParqueaderoPrivado(nombre, metrosCuadrados, necesitaMantenimiento, alicuota, propietario);
     }
+
     
     public Departamento(String nombre, double metrosCuadrados) {
         super(nombre, metrosCuadrados,false, 0, null);
+        this.parqueaderPrivado = new ParqueaderoPrivado(nombre, metrosCuadrados, necesitaMantenimiento, alicuota, propietario);
     }
 
     public Departamento(double metrosCuadrados) {
         super(null, metrosCuadrados,false, 0, null);
+        this.parqueaderPrivado = new ParqueaderoPrivado(null,metrosCuadrados, necesitaMantenimiento, alicuota, propietario);
     }
 }
