4de6cd3865535dd63f8a59c0f58799fce891366e
Santiago Bejarano <santiago.bejarano@epn.edu.ec>
2024-02-19 00:57:12 -0500

    Pruebas en Main para Reserva Areas Comunes

--- ProyectoCondominio/src/main/java/Main.java
+++ ProyectoCondominio/src/main/java/Main.java
@@ -5,6 +5,11 @@ import Administracion.Directiva;
 import Administracion.Residente;
 import Inmueble.Condominio;
 import Inmueble.Gimnasio;
+import Inmueble.*;
+import areaComun.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
@@ -28,7 +33,27 @@ public class Main {
         //Pruebas funcionales modulo 1 comunicacion
         //Pruebas funcionales modulo 2 comunicacion
         //Pruebas funcionales modulo 3 comunicacion
-        //Pruebas funcionales modulo 4 comunicacion
+        //Pruebas funcionales modulo 4 Reservas Areas Comunes
+        // Creamos un usuario de tipo Administrador
+        Administrador usuarioReserva = new Administrador("pepe.julio@epn.edu.ec", "acabenmodulos", "Pepe Julio", new Condominio ("Fis EPN"));
+        
+        // Creamos una lista de reservas
+        List<Reserva> listaReservas = new ArrayList<>();
+        
+        // Creamos una lista de días disponibles
+        List<String> diasDisponibles = Arrays.asList("Lunes", "Martes", "Miércoles", "Jueves", "Viernes");
+        
+        // Crear un área común tipo Cancha con sus atributos
+        Cancha areaComunReserva = new Cancha(listaReservas, diasDisponibles, "cancha1", 100, true, 100, usuarioReserva);
+
+        // Creamos el Administrador de las Reservas
+        AdministrarReserva adminReserva = new AdministrarReserva();
+
+        // Creamos una Reserva para la Cancha1
+        String diaReserva = "lunes";
+        String detalleReserva = "Futbolito";
+        Reserva nuevaReserva = adminReserva.realizarReserva(areaComunReserva, diaReserva, detalleReserva, usuarioReserva);
+        
         //Pruebas funcionales modulo 5 comunicacion
         //Pruebas funcionales modulo 6 comunicacion
             //REQ_MA_CU-ManejarCondominio_01
