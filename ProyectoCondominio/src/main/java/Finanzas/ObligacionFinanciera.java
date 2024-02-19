772e81fb1d377c0b580d035eb7fff21129f1b3e5
BryanGuano02 <bryan.guano@epn.edu.ec>
2024-02-18 23:21:31 -0500

    agregar codigo

--- ProyectoCondominio/src/main/java/Finanzas/ObligacionFinanciera.java
+++ ProyectoCondominio/src/main/java/Finanzas/ObligacionFinanciera.java
@@ -8,6 +8,49 @@ package Finanzas;
  *
  * @author alejo
  */
-public class ObligacionFinanciera {
-    
+import java.time.LocalDate;
+
+public abstract class ObligacionFinanciera {
+    private EstadoObligacion estado;
+    private double monto;
+    private LocalDate fechaCreacion;
+    private String descripcion;
+    private String idObligacion;
+
+    public ObligacionFinanciera(double valor, LocalDate fechaCreacion, String descripcion, String idObligacion) {
+        monto = calcularMonto(valor);
+        this.fechaCreacion = fechaCreacion;
+        estado = new EstadoPendiente();
+        this.descripcion = descripcion;
+        this.idObligacion = idObligacion;
+    }
+
+    public abstract double calcularMonto(double monto);
+/// TODO otro constructor para la multa
+
+    public void cambiarEstado() {
+        estado.cambiarEstado(this);
+    }
+
+
+    public void setEstado(EstadoObligacion estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return "id obligacion: " + idObligacion +
+                ", estado = " + estado +
+                ", monto= " + monto +
+                ", fechaCreacion= " + fechaCreacion +
+                ", descripcion= '" + descripcion + "'";
+    }
+
+    public double getMonto() {
+        return monto;
+    }
+
+    public String getIdObligacion() {
+        return idObligacion;
+    }
 }
