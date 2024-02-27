70d9a0deff4b01752c114523c83e97e34d1c1fcc
BryanGuano02 <bryan.guano@epn.edu.ec>
2024-02-26 23:06:32 -0500

    update GR2 se actualizó la clase EstadoCompletado

--- ProyectoCondominio/src/main/java/Finanzas/EstadoCompletado.java
+++ ProyectoCondominio/src/main/java/Finanzas/EstadoCompletado.java
@@ -1,15 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Finanzas;
 
 import java.io.Serializable;
 
-/**
- *
- * @author alejo
- */
 public class EstadoCompletado implements EstadoObligacion, Serializable {
 
     @Override
