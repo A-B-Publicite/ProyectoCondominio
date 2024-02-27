2adfe3941c41f826c4365a417e5bd0fd6c49f549
BryanGuano02 <bryan.guano@epn.edu.ec>
2024-02-26 23:05:31 -0500

    update GR2 se actualizó la clase EstadoAtrasado

--- ProyectoCondominio/src/main/java/Finanzas/EstadoAtrasado.java
+++ ProyectoCondominio/src/main/java/Finanzas/EstadoAtrasado.java
@@ -1,15 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Finanzas;
 
 import java.io.Serializable;
 
-/**
- *
- * @author alejo
- */
 public class EstadoAtrasado implements EstadoObligacion, Serializable {
     @Override
     public void cambiarEstado(ObligacionFinanciera obligacionFinanciera, String senial) {
