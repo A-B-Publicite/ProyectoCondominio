997cf74412bd093f2c4623929e410abfd75bfcc7
Kevin Sebastian D <kedose@outlook.it>
2024-02-29 03:32:25 -0500

    J

--- ProyectoCondominio/src/main/java/Mueble/AdministracionAlquiler.java
+++ ProyectoCondominio/src/main/java/Mueble/AdministracionAlquiler.java
@@ -1,14 +1,57 @@
 package Mueble;
+
 import java.util.ArrayList;
 
 public class AdministracionAlquiler {
-    ArrayList<Alquiler> listaAlquileres = new ArrayList<Alquiler>();
-    
-    public void alquilar(){
-        
+
+    ArrayList<Alquiler> listaAlquileres;
+
+    public AdministracionAlquiler() {
+        listaAlquileres = new <Alquiler>ArrayList();
     }
-    
-    public void verificarDevolucion(){
-        
+
+    public AdministracionAlquiler(ArrayList<Alquiler> listaAlquileres) {
+        this.listaAlquileres = listaAlquileres;
     }
+
+    public ArrayList<Alquiler> getListaAlquileres() {
+        return listaAlquileres;
+    }
+
+    public void setListaAlquileres(ArrayList<Alquiler> listaAlquileres) {
+        this.listaAlquileres = listaAlquileres;
+    }
+
+    public boolean estaVacia() {
+        return listaAlquileres.isEmpty();
+    }
+
+    public void add(Alquiler alquiler) {
+        listaAlquileres.addLast(alquiler);
+    }
+
+    public void alquilar() {
+
+    }
+
+    public void verificarDevolucion() {
+
+    }
+
+    @Override
+    public String toString() {
+        String salida = "Lista de Muebles Alquilados: ";
+        int i = 1;
+        if (estaVacia()) {
+            return "Lista vacía";
+        } else {
+            for (Alquiler aux : listaAlquileres) {
+                salida += "\nAlquiler " + i;
+                salida += aux.toString();
+                i++;
+            }
+            return salida;
+        }
+    }
+
 }
