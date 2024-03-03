c8c3f57b95aa1fd5c993a0eb064005e0ec5693d1
Kevin Sebastian D <kedose@outlook.it>
2024-03-03 12:28:06 -0500

    Gr3 feat Agregación de métodos clase Disponibilidad

--- ProyectoCondominio/src/main/java/ModuloMuebles/Disponibilidad.java
+++ ProyectoCondominio/src/main/java/ModuloMuebles/Disponibilidad.java
@@ -1,12 +1,25 @@
-
 package ModuloMuebles;
 
-/**
- *
- * @author Grupo3
- */
 public class Disponibilidad {
-    private Mueble mueble ;
-    
-    
+
+    private Mueble mueble;
+
+    public Disponibilidad(Mueble mueble) {
+        this.mueble = mueble;
+    }
+
+    public Mueble getMueble() {
+        return mueble;
+    }
+
+    public void setMueble(Mueble mueble) {
+        this.mueble = mueble;
+    }
+
+    public double verificarDisponibilidad() {
+    }
+
+    public double actualizarDisponibilidad() {
+    }
+
 }
