4b14ff37d6ad117d0cb7ccda358ba084ba0c9d3b
Kevin Sebastian D <kedose@outlook.it>
2024-03-03 16:50:53 -0500

    Gr3 feat actualizarDisponibilidad

--- ProyectoCondominio/src/main/java/ModuloMuebles/Inventario.java
+++ ProyectoCondominio/src/main/java/ModuloMuebles/Inventario.java
@@ -38,73 +38,22 @@ public class Inventario implements Serializable {
     public boolean verificarDisponibilidad(String tipoMueble, int cantidad) {
         int count = 0;
         for (Mueble mueble : listaMuebles) {
-            if (mueble.esTipo(tipoMueble)) {
-                count ++;
+            if (mueble.esTipo(tipoMueble) && mueble.isEstado()) {
+                count++;
             }
+            return count == cantidad;
         }
-        return count >= cantidad;
+        return false;
     }
 
     public void actualizarDisponibilidad(String tipoMueble, int cantidad, boolean finalizacion) {
-        switch (tipoMueble) {
-            case "Carpa" -> {
-                int count1 = 0;
-                int count2 = 0;
-                for (Mueble mueble : listaMuebles) {
-                    if (mueble instanceof Carpa) {
-                        if (mueble.isEstado()) {
-                            if (count1 < cantidad && devolucion == false) {
-                                count1++;
-                                mueble.setEstado(false);
-                            }
-                        } else {
-                            if (count2 < cantidad && devolucion == true) {
-                                count2++;
-                                mueble.setEstado(true);
-                            }
-                        }
-                    }
-                }
-                break;
-            }
-            case "Silla" -> {
-                int count1 = 0;
-                int count2 = 0;
-                for (Mueble mueble : listaMuebles) {
-                    if (mueble instanceof Silla) {
-                        if (mueble.isEstado()) {
-                            if (count1 < cantidad && devolucion == false) {
-                                count1++;
-                                mueble.setEstado(false);
-                            }
-                        } else {
-                            if (count2 < cantidad && devolucion == true) {
-                                count2++;
-                                mueble.setEstado(true);
-                            }
-                        }
-                    }
-                }
-                break;
-            }
-            case "Mesa" -> {
-                int count1 = 0;
-                int count2 = 0;
-                for (Mueble mueble : listaMuebles) {
-                    if (mueble.isEstado()) {
-                        if (count1 < cantidad && devolucion == false) {
-                            count1++;
-                            mueble.setEstado(false);
-                        }
-                    } else {
-                        if (count2 < cantidad && devolucion == true) {
-                            count2++;
-                            mueble.setEstado(true);
-                        }
-                    }
-                }
-                break;
+        int count = 0;
+        for (Mueble mueble : listaMuebles) {
+            if (mueble.esTipo(tipoMueble) && count <= cantidad) {
+                count++;
+                mueble.setEstado(finalizacion);
             }
         }
     }
+
 }
