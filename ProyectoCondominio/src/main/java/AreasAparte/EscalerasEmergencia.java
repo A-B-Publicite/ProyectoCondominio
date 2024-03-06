7ef00675e68c28521bd6820c4a913d16ed63a981
paoisbllxx22 <155592052+paoisbllxx22@users.noreply.github.com>
2024-03-05 20:22:12 -0500

    Feat AreasAparte
    Agregación de la clase padre AreasDeTransito con sus dos hijos
     ascensor y escalerasEmergencia

---
+++ ProyectoCondominio/src/main/java/AreasAparte/EscalerasEmergencia.java
@@ -0,0 +1,16 @@
+
+package AreasAparte;
+
+import java.io.Serializable;
+
+
+public class EscalerasEmergencia extends AreaDeTransito implements Serializable{
+ 
+    public EscalerasEmergencia(boolean necesitaMantenimiento) {
+        super(necesitaMantenimiento);
+    }
+ 
+    
+    
+    
+}
